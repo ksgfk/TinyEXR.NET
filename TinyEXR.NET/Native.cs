0cf1ab84e4576ae11f74c08949f1645700a4b3cf
ksgfk <1537100607@qq.com>
2022-07-13 23:14:02 +0800

    wrap native dynamic library

---
+++ TinyEXR.NET/Native.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TinyEXR
+{
+    public unsafe static class Native
+    {
+        public const string LibraryName = "TinyEXR.NET.Native";
+
+        [DllImport(LibraryName, CharSet = CharSet.Unicode)]
+        private static extern int LoadEXR_Export(float** out_rgba, int* width, int* height, char* filename, char** err);
+    }
+}
