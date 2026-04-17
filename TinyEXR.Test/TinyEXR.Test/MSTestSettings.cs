93ace5a56cd1c6568624658593843646aef57af9
ksgfk <1537100607@qq.com>
2026-04-17 22:28:00 +0800

    test: cover spectral helpers and pxr24 writer

--- TinyEXR.Test/TinyEXR.Test/MSTestSettings.cs
+++ TinyEXR.Test/TinyEXR.Test/MSTestSettings.cs
@@ -1,1 +1,1 @@
-﻿[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+[assembly: Parallelize(Workers = 1, Scope = ExecutionScope.MethodLevel)]
--- TinyEXR.Test/TinyEXR.Test/SimpleTest.cs
+++ TinyEXR.Test/TinyEXR.Test/SimpleTest.cs
@@ -1,4 +1,8 @@
-﻿namespace TinyEXR.Test
+using System.Runtime.InteropServices;
+using System.Text;
+using TinyEXR.Native;
+
+namespace TinyEXR.Test
 {
     [TestClass]
     public sealed class SimpleTest
@@ -47,9 +51,130 @@
                     rgba[idx + 3] = 1.0f;
                 }
             }
+
             ResultCode rc = Exr.SaveEXRToMemory(rgba, width, height, components, false, out byte[] result);
             Assert.AreEqual(ResultCode.Success, rc);
             Assert.AreEqual(11243, result.Length);
+        }
+
+        [TestMethod]
+        public unsafe void SpectralHelpers()
+        {
+            Assert.AreEqual("550,000000", Exr.EXRFormatWavelength(550.0f));
+
+            string reflective = Exr.EXRReflectiveChannelName(650.0f);
+            string polarised = Exr.EXRSpectralChannelName(550.0f, 2);
+
+            Assert.AreEqual("T.650,000000nm", reflective);
+            Assert.AreEqual("S2.550,000000nm", polarised);
+            Assert.AreEqual(650.0f, Exr.EXRParseSpectralChannelWavelength(reflective), 0.001f);
+            Assert.AreEqual(550.0f, Exr.EXRParseSpectralChannelWavelength(polarised), 0.001f);
+            Assert.AreEqual(-1, Exr.EXRGetStokesComponent(reflective));
+            Assert.AreEqual(2, Exr.EXRGetStokesComponent(polarised));
+            Assert.IsTrue(Exr.EXRIsSpectralChannel(reflective));
+            Assert.IsTrue(Exr.EXRIsSpectralChannel(polarised));
+
+            EXRHeader header = default;
+            try
+            {
+                Exr.InitEXRHeader(ref header);
+
+                ResultCode rc = Exr.EXRSetSpectralAttributes(ref header, SpectrumType.Polarised, "W.m^-2.sr^-1.nm^-1");
+                Assert.AreEqual(ResultCode.Success, rc);
+                Assert.AreEqual("W.m^-2.sr^-1.nm^-1", Exr.EXRGetSpectralUnits(ref header));
+
+                EXRChannelInfo[] channels = new EXRChannelInfo[3];
+                SetChannelName(ref channels[0], Exr.EXRSpectralChannelName(650.0f, 0));
+                SetChannelName(ref channels[1], Exr.EXRSpectralChannelName(550.0f, 1));
+                SetChannelName(ref channels[2], Exr.EXRSpectralChannelName(550.0f, 0));
+
+                fixed (EXRChannelInfo* channelsPtr = channels)
+                {
+                    header.channels = channelsPtr;
+                    header.num_channels = channels.Length;
+
+                    Assert.AreEqual(SpectrumType.Polarised, Exr.EXRGetSpectrumType(ref header));
+
+                    float[] wavelengths = Exr.EXRGetWavelengths(ref header);
+                    Assert.AreEqual(2, wavelengths.Length);
+                    Assert.AreEqual(550.0f, wavelengths[0], 0.001f);
+                    Assert.AreEqual(650.0f, wavelengths[1], 0.001f);
+
+                    header.channels = null;
+                    header.num_channels = 0;
+                }
+            }
+            finally
+            {
+                Exr.FreeEXRHeader(ref header);
+            }
+        }
+
+        [TestMethod]
+        public void WriterSupportsPXR24Compression()
+        {
+            const int width = 8;
+            const int height = 4;
+            const int pixelCount = width * height;
+
+            float[] r = new float[pixelCount];
+            float[] g = new float[pixelCount];
+            float[] b = new float[pixelCount];
+            float[] a = new float[pixelCount];
+
+            for (int i = 0; i < pixelCount; i++)
+            {
+                r[i] = i / (float)pixelCount;
+                g[i] = 1.0f - r[i];
+                b[i] = (i % width) / (float)width;
+                a[i] = 1.0f;
+            }
+
+            ScanlineExrWriter writer = new ScanlineExrWriter()
+                .SetSize(width, height)
+                .SetCompression(CompressionType.PXR24)
+                .AddChannel("R", ExrPixelType.Float, ToBytes(r), ExrPixelType.Float)
+                .AddChannel("G", ExrPixelType.Float, ToBytes(g), ExrPixelType.Float)
+                .AddChannel("B", ExrPixelType.Float, ToBytes(b), ExrPixelType.Float)
+                .AddChannel("A", ExrPixelType.Float, ToBytes(a), ExrPixelType.Float);
+
+            byte[] result = writer.Save()!;
+
+            ResultCode versionRc = Exr.ParseEXRVersionFromMemory(result, out EXRVersion version);
+            Assert.AreEqual(ResultCode.Success, versionRc);
+
+            EXRHeader header = default;
+            try
+            {
+                Exr.InitEXRHeader(ref header);
+                ResultCode headerRc = Exr.ParseEXRHeaderFromMemory(result, ref version, ref header);
+                Assert.AreEqual(ResultCode.Success, headerRc);
+                Assert.AreEqual((int)CompressionType.PXR24, header.compression_type);
+            }
+            finally
+            {
+                Exr.FreeEXRHeader(ref header);
+            }
+        }
+
+        private static byte[] ToBytes(float[] values)
+        {
+            byte[] bytes = new byte[values.Length * sizeof(float)];
+            MemoryMarshal.AsBytes(values.AsSpan()).CopyTo(bytes);
+            return bytes;
+        }
+
+        private static unsafe void SetChannelName(ref EXRChannelInfo channel, string name)
+        {
+            fixed (sbyte* namePtr = channel.name)
+            {
+                Span<byte> buffer = new Span<byte>(namePtr, 256);
+                buffer.Clear();
+
+                int byteCount = Encoding.UTF8.GetByteCount(name);
+                Assert.IsTrue(byteCount < buffer.Length);
+                Encoding.UTF8.GetBytes(name, buffer[..byteCount]);
+            }
         }
     }
 }
